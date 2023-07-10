c7bda8f6445d93ff21aa144ed3e3a0ba71b9099b
Risqi Ardiansyah <ardiris19@gmail.com>
2023-07-10 08:41:43 +0700

    build

---
+++ build/precache-manifest.977cc308a694c1930207037107489644.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e3f3922a608c0d08b3c8c227ad598a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796ef76d6e97cc2d9b3c",
+    "url": "/static/css/4.6480a25c.chunk.css"
+  },
+  {
+    "revision": "aaa1bb1df091ec04c2a8",
+    "url": "/static/css/main.f1c19270.chunk.css"
+  },
+  {
+    "revision": "a025ca4baac836306b5d",
+    "url": "/static/js/0.49b6e98f.chunk.js"
+  },
+  {
+    "revision": "73c76aefaef64fb1f6d9",
+    "url": "/static/js/1.25552288.chunk.js"
+  },
+  {
+    "revision": "796ef76d6e97cc2d9b3c",
+    "url": "/static/js/4.7349525d.chunk.js"
+  },
+  {
+    "revision": "3cb702e04014f3af261d1d953f4236e6",
+    "url": "/static/js/4.7349525d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344a330a580e16a5addb",
+    "url": "/static/js/5.16df8e96.chunk.js"
+  },
+  {
+    "revision": "86a34ab3420c99712db0",
+    "url": "/static/js/6.716e5d8e.chunk.js"
+  },
+  {
+    "revision": "aaa1bb1df091ec04c2a8",
+    "url": "/static/js/main.2e51463c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2e51463c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8641895e7b2427caa0d7",
+    "url": "/static/js/runtime-main.72d86187.js"
+  },
+  {
+    "revision": "9177faa11908ab9a07cbe18634af1ed7",
+    "url": "/static/media/Aksieye.9177faa1.svg"
+  },
+  {
+    "revision": "9a39b4aecd176ed10c34d19bf3208795",
+    "url": "/static/media/IcDot.9a39b4ae.svg"
+  },
+  {
+    "revision": "ad6b065d9588c57c6c1f09a3172f31c1",
+    "url": "/static/media/ic_bin.ad6b065d.svg"
+  },
+  {
+    "revision": "f0a428a2583f9c9842468e90a61c21ef",
+    "url": "/static/media/ic_calendar.f0a428a2.svg"
+  },
+  {
+    "revision": "6d292554346985c4a82240d6711299af",
+    "url": "/static/media/ic_delete.6d292554.svg"
+  },
+  {
+    "revision": "8038e5b5a99402efa1c5d4f0f9cd8a1b",
+    "url": "/static/media/ic_input.8038e5b5.svg"
+  },
+  {
+    "revision": "8dfd625fc2ce36e6e88e54c2e20ebdde",
+    "url": "/static/media/ic_money.8dfd625f.svg"
+  },
+  {
+    "revision": "51b017870669cfbaf6d6865d1fb24430",
+    "url": "/static/media/ic_plus.51b01787.svg"
+  },
+  {
+    "revision": "4489861d584ce7f96fcbd8036b36355e",
+    "url": "/static/media/ic_topup.4489861d.svg"
+  }
+]);
